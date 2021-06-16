9627d36ec761f207796590053ac3a2e7597d8d34
Wellington Garcia <garciawell@gmail.com>
2021-06-16 18:50:13 -0300

    add route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Video from './components/Video';
+import { io } from "socket.io-client";
+import { joinRoom } from "./socket";
+import { useParams } from 'react-router-dom';
+
+
+
+let socket;
 
 function App() {
-  const videoRef = useRef();
+  const videoRef = useRef(null);
+  const [text, setText] = useState("");
+  let { id } = useParams();
 
   useEffect(() => {
     const getMediaDevices = async () => {
@@ -17,10 +26,43 @@ function App() {
     getMediaDevices()
   }, [])
 
+  useEffect(() => {
+     socket = io("http://localhost:3333", {transports: ['websocket']});
+  }, [])
+
+  function handleJoinRoom() {
+    socket.emit('create', id);
+  }
+  
+
+  useEffect(() => {
+    socket.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    });
+
+    socket.emit('create', id);
+
+    socket.on("connect_error", (error) => {
+      console.log("ERROR", error)
+    });
+
+    socket.on(id, (data) => { 
+      console.log("OPAAA2", data)
+      setText(data);
+    });
+  }, [id])
+  
+  
   return (
     <div className="App">
+
       <header className="App-header">
-          <Video playInline autoPlay ref={videoRef} />
+          <Video ref={videoRef} />
+          OPS: {text}
+          <button onClick={handleJoinRoom}>Logar</button>
       </header>
     </div>
   );
