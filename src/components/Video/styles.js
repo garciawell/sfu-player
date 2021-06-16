a6df534da9bf5ab023d686a917af7ae0c32e739f
Wellington Garcia <garciawell@gmail.com>
2021-06-16 11:35:16 -0300

    Get video and audio

---
+++ src/components/Video/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    video {
+        /* background: rgba(0,0,0,0.2); */
+        width: 500px;
+        height: 500px;
+    }
+`;
