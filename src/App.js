2df14cfe7d63d08cd3f8e9ea4f6ea9bfd24a2572
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-06-16 11:35:16 -0300

    Get video and audio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
+import Video from './components/Video';
 
 function App() {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    const getMediaDevices = async () => {
+      const localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      })
+      videoRef.current.srcObject = localStream
+    }
+  
+    getMediaDevices()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-          <h1>OLA MUNDO</h1>
+          <Video playInline autoPlay ref={videoRef} />
       </header>
     </div>
   );
---
+++ src/components/Video/index.js
@@ -0,0 +1,15 @@
+import React, { forwardRef } from 'react';
+
+import { Container } from './styles';
+
+const Video = forwardRef((props, videoRef) => { 
+  return (
+    <Container>
+        <h1>OLA MUNDO</h1>
+
+        <video muted ref={videoRef} {...props} />
+    </Container>
+  );
+})
+
+export default Video;
